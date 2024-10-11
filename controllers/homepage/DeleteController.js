411a76f91f3de2e041c91041283ed35ec366b87e
JoshVilla <villaearljoshua@gmail.com>
2024-10-11 21:03:36 +0800

    fix deleting image in cloudinary

--- controllers/homepage/DeleteController.js
+++ controllers/homepage/DeleteController.js
@@ -5,9 +5,9 @@ const deleteHomePageInfo = async (req, res) => {
   const { section, idSectionData, imagePublicId } = req.body;
 
   const deleteImageFromCloudinary = async (imagePublicId) => {
-    console.log(imagePublicId, "@@@@@@@@@imagePublicId");
+    const publicId = imagePublicId.split(".").shift();
 
-    cloudinary.uploader.destroy(imagePublicId);
+    cloudinary.uploader.destroy(publicId);
   };
   deleteImageFromCloudinary(imagePublicId);
   HomepageModel.findByIdAndUpdate("67027185ee9f3ce34598e2c4", {

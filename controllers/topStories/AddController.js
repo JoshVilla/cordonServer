7b4d2dacbbd1781cef35c3ee82138bd6c8b9dcb1
JoshVilla <villaearljoshua@gmail.com>
2024-10-15 21:11:55 +0800

    fix bug

--- controllers/topStories/AddController.js
+++ controllers/topStories/AddController.js
@@ -2,31 +2,35 @@ const TopStoriesModel = require("../../models/TopStories");
 const cloudinary = require("../../config/cloudinaryConfig");
 
 const addStory = async (req, res) => {
+  console.log("Received Data:", req.body);
   try {
-    const { title, thumbnail, items } = req.body;
+    const { title, items } = req.body;
 
-    const parsedItems = JSON.parse(items);
-    console.log(req.files);
+    // Ensure `items` is properly parsed if it's sent as a JSON string
+    // const parsedItems = typeof items === "string" ? JSON.parse(items) : items;
+    // console.log("Parsed Items:", parsedItems);
+
+    let imageUrl = ""; // Placeholder for the thumbnail URL
 
     // Upload the file to Cloudinary if it exists
-    if (req.files?.thumbnail && req.files.thumbnail[0]) {
-      const thumbnailFile = req.files.thumbnail[0]; // Access the first file in thumbnail array
-      const uploadResult = await cloudinary.uploader.upload(
-        thumbnailFile.path,
-        {
-          folder: "admin_stories",
-        }
-      );
-      imageUrl = uploadResult.secure_url;
-    }
-
-    console.log(imageUrl);
+    // const uploadedItems = await Promise.all(
+    //   parsedItems.map(async (item) => {
+    //     if (item.type === "image" && req.files[`items[${index}].image`]) {
+    //       const imageUpload = await cloudinary.uploader.upload(
+    //         req.files[`items[${index}].image`][0].path,
+    //         { folder: "admin_stories" }
+    //       );
+    //       return { ...item, image: imageUpload.secure_url };
+    //     }
+    //     return item;
+    //   })
+    // );
 
     const getPublicIdForCloudinary = (fileUrl) => {
       if (fileUrl) {
         const splitted = fileUrl.split("/");
-        const public = `${splitted[7]}/${splitted[8]}`;
-        const publicId = public.replace(".png", "");
+        const publicPart = `${splitted[7]}/${splitted[8]}`;
+        const publicId = publicPart.replace(".png", "");
         return publicId;
       }
       return "";
@@ -36,19 +40,20 @@ const addStory = async (req, res) => {
       title,
       thumbnail: imageUrl,
       thumbnailPublicId: getPublicIdForCloudinary(imageUrl),
-      content: parsedItems, // Items will now be an array of objects
+      content: [], // Parsed items should now be an array of objects
     };
 
-    console.log(params);
+    console.log("Params:", params);
 
-    // You can now save `params` to the database using `TopStoriesModel`
-    await TopStoriesModel.create(params);
+    // Save the story to the database
+    // await TopStoriesModel.create(params);
 
-    // Send a response confirming the body was processed
+    // Send a success response
     return res
       .status(200)
       .json({ message: "Story added successfully", data: params });
   } catch (err) {
+    console.error("Error adding story:", err);
     return res.status(500).json({ error: err.message });
   }
 };

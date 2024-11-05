2619f73e0486afe50b74dad83695ae942a33dc78
JoshVilla <villaearljoshua@gmail.com>
2024-11-05 20:01:08 +0800

    add pagination component

--- controllers/topStories/GetController.js
+++ controllers/topStories/GetController.js
@@ -4,16 +4,15 @@ import moment from "moment";
 const getTopStories = async (req, res) => {
   const { title, dates, id } = req.body;
   const params = {};
-  console.log(req.body);
-  // Add title filter if provided
+
   if (title) params.title = title;
   if (id) params._id = id;
-  // Add date range filter if provided and valid
+
   const { start, end } = dates || {};
   if (start && end) {
     params.createdAt = {
-      $gte: moment(start).format("MMMM D, YYYY"), // Start of the day
-      $lte: moment(end).format("MMMM D, YYYY"), // End of the day
+      $gte: moment(start).format("MMMM D, YYYY"),
+      $lte: moment(end).format("MMMM D, YYYY"),
     };
   }
 

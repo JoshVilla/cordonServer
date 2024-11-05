c247f3fd8a411859119572924b9ef15bd0270b4d
JoshVilla <villaearljoshua@gmail.com>
2024-11-05 20:41:24 +0800

    add pagination param

--- controllers/topStories/GetController.js
+++ controllers/topStories/GetController.js
@@ -2,8 +2,10 @@ import TopStoriesModel from "../../models/TopStories.js";
 import moment from "moment";
 
 const getTopStories = async (req, res) => {
-  const { title, dates, id } = req.body;
+  const { title, dates, id, page = 1, limit = 10 } = req.body; // Set default values for page and limit
   const params = {};
+  const pageSize = parseInt(limit, 10); // Number of items per page
+  const pageNumber = parseInt(page, 10); // Current page number
 
   if (title) params.title = title;
   if (id) params._id = id;
@@ -17,9 +19,21 @@ const getTopStories = async (req, res) => {
   }
 
   try {
-    const result = await TopStoriesModel.find(params);
+    // Count total documents based on filters
+    const totalData = await TopStoriesModel.countDocuments(params);
 
-    return res.status(200).json(result);
+    // Apply pagination
+    const result = await TopStoriesModel.find(params)
+      .skip((pageNumber - 1) * pageSize) // Skip previous pages
+      .limit(pageSize); // Limit to page size
+
+    return res.status(200).json({
+      data: result,
+      totalData,
+      pageSize: result.length,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(totalData / pageSize),
+    });
   } catch (err) {
     console.error("Error fetching top stories:", err);
     return res
